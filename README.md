0912f0e533da7d04bc94160f7a1835f4cbacaa1b
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2023-03-13 12:49:03 +0000

    Strings, funciones para comparar strcmp y strcasecmp.

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Se seguirá el orden de los videos desde el primero al último y se realizarán 
 - Video 5: Flujo de ejecución, declaración y llamadas de funciones en PHP.
 - Video 6: Ámbito de las variables. Local y global. Falta el superglobal, se verá más adelante cuando veamos los arrays.
 - Video 7: Variables estáticas.
+- Video 8: Strings. Declaración, asignación, uso de comillas simples y dobles, y carácteres escapados. 
+           Uso de las funciones strcmp y strcasecmp para comparar strings. E introducción del condicional if/else.
--- ejemplo_string.php
+++ ejemplo_string.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Strings</title>
-    
+
     <style>
         .resaltar {
             color: red;
@@ -14,20 +15,51 @@
     </style>
 
 </head>
+
 <body>
 
-    <?php 
+    <?php
 
-        echo "<p class='resaltar'>Esto es un ejemplo para usar las comillas simples o el apóstrofe.</p>"; 
+        echo "<p class='resaltar'>Esto es un ejemplo para usar las comillas simples o el apóstrofe.</p>";
         /* No se puede usar comillas dobles (") dentro de comillas dobles. 
-        En caso necesario utilizaremos las comillas simples dentro de las dobles como en párrafo del ejemplo. 
-        O comillas dobles dentro de comillas simples, es decir, 
-        NO PUEDEN HABER COMILLAS HIJAS Y PADRES DEL MISMO TIPO */
+            En caso necesario utilizaremos las comillas simples dentro de las dobles como en párrafo del ejemplo. 
+            O comillas dobles dentro de comillas simples, es decir, 
+            NO PUEDEN HABER COMILLAS HIJAS Y PADRES DEL MISMO TIPO */
 
-        echo "<p class=\"resaltar \"> Esto es un ejemplo de como escapar las comillas dobles para poder usarlas como hijos y padres";
+        echo "<p class=\"resaltar \"> Esto es un ejemplo de como escapar las comillas dobles para poder usarlas como hijos y padres </p>";
         /* Para escapar se utiliza la barra invertida \ */
 
     ?>
-    
+
+    <?php
+
+        $var1 = "casa";
+        $var2 = "CASA";
+
+        $resultado = strcmp($var1, $var2); /* Devuelve 1 si NO son iguales, Devuelve 0 si SÍ son iguales. 
+                                              Esta función SI DIFERENCIA entre mayúsculas y minúsculas.  */
+        echo "El resultado es $resultado <br>";
+
+        if (!$resultado){
+            echo "Sí coinciden <br>";
+        }else{
+            echo "No coinciden <br>";
+        }
+        
+        $resultado = strcasecmp($var1, $var2); /* Devuelve 1 si NO son iguales, Devuelve 0 si SÍ son iguales. 
+                                              Esta función NO DIFERENCIA entre mayúsculas y minúsculas.  */
+        echo "El resultado es $resultado <br>";
+
+        if (!$resultado){
+            echo "Sí coinciden <br>";
+        }else{
+            echo "No coinciden <br>";
+        }
+
+
+
+    ?>
+
 </body>
+
 </html>
