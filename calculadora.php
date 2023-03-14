1c2052dde9b924429c10236f696531fdc5aef8e5
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2023-03-14 13:29:04 +0000

    ejercicios de operadores matemáticos,  archivos html y php, llamadas a funciones

---
+++ calculadora.php
@@ -0,0 +1,57 @@
+<style>
+    .resultado {
+        color: red;
+        font-weight: bold;
+        font-size: 42px;
+    }
+</style>
+
+<?php
+
+if (isset($_POST["button"])) {
+
+    $numero1 = $_POST["num1"];
+    $numero2 = $_POST["num2"];
+    $operacion = $_POST["operacion"];
+
+    calcular($operacion);
+}
+
+function calcular($calculo) {
+
+    if (!strcmp("Suma", $calculo)) {
+        global $numero1;
+        global $numero2;
+        $resultado = $numero1 + $numero2;
+        echo "<p class=\"resultado\">El resultado es: $resultado </p>";
+    }
+
+    if (!strcmp("Resta", $calculo)) {
+        global $numero1;
+        global $numero2;
+        $resultado = $numero1 - $numero2;
+        echo "<p class=\"resultado\">El resultado es: $resultado </p>";
+    }
+
+    if (!strcmp("Multiplicación", $calculo)) {
+        global $numero1;
+        global $numero2;
+        $resultado = $numero1 * $numero2;
+        echo "<p class=\"resultado\">El resultado es: $resultado </p>";
+    }
+
+    if (!strcmp("División", $calculo)) {
+        global $numero1;
+        global $numero2;
+        $resultado = $numero1 / $numero2;
+        echo "<p class=\"resultado\">El resultado es: $resultado </p>";
+    }
+    if (!strcmp("Módulo", $calculo)) {
+        global $numero1;
+        global $numero2;
+        $resultado = $numero1 % $numero2;
+        echo "<p class=\"resultado\">El resultado es: $resultado </p>";
+    }
+}
+
+?>
