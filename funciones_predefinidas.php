85097aa25a0ae406fdc40912a0230a6ee73f2ac3
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2023-04-12 07:52:27 +0100

    Ejemplo de funciones propias, funciones anidadas y paso por parámetros.

--- funciones_predefinidas.php
+++ funciones_predefinidas.php
@@ -17,6 +17,16 @@
     echo (strtolower($palabra)) . "<br>";
     echo (strtoupper($palabra)) . "<br>";
 
+
+    function suma($num1, $num2)
+    {
+        $resultado = $num1 + $num2;
+        return $resultado;
+    }
+
+    echo("El resultado de la suma es " . suma(5,7));
+
+
     ?>
 
 </body>
