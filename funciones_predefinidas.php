65bd142310f0f0d90fa9ccbc8b0ef4483ede28a7
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2023-04-12 07:42:18 +0100

    Ejemplo de funciones predefinidas, strlower y strupper

---
+++ funciones_predefinidas.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funciones predefinidas</title>
+</head>
+
+<body>
+
+    <?php
+
+    $palabra = "Pepe";
+    echo ($palabra) . "<br>";
+    echo (strtolower($palabra)) . "<br>";
+    echo (strtoupper($palabra)) . "<br>";
+
+    ?>
+
+</body>
+
+</html>
