c16b8084d48a6b346a44ded97ac3d075f289a738
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2023-10-26 18:22:51 +0100

    Como agrergar un elemento a un array asociativo.

--- arrays.php
+++ arrays.php
@@ -10,10 +10,12 @@
 <body>
 
     <?php
-
-        /* ARRAYS INDEXADOS */
+        /*********************************/
+        /*     ARRAYS INDEXADOS          */
+        /*********************************/
         /* PHP permite no poner el indice a la hora de declarar los arrays */
         /* Existen varias formas de sintaxis para la creación de arrays */
+
         $entreSemana[] = "Lunes"; //declaración de array, indice 0, valor "Lunes"
         $entreSemana[] = "Martes"; //declaración de array, indice 1, valor "Martes"
         $entreSemana[] = "Miércoles"; //declaración de array, indice 2, valor "Miercoles"
@@ -26,30 +28,11 @@
         echo $entreSemana[2] . "<br>"; // imprime por pantalla la posición 1, es decir el indice 0.
         echo $finDeSemana[0] . "<br>";
 
-        /* ARRAYS ASOCIATIVOS O NO INDEXADOS */
-
-        $datos = array("Nombre" => "José", "Apellido" => "Rodriguez", "Edad" => 50); //declaración de array asociativo.
-        echo $datos["Nombre"] . "<br>"; //Imprime por pantalla la posición Nombre. Es decir el valor José.
-        /*  $datos ="Juanito"; */
-
-        //la función predefinida is_array es una función booleana que sirve para comprobar si el parámetro introducido es o no es un array.
-        if (is_array($datos) == true) {
-            echo ("Es un array <br>");
-        } else {
-            echo ("No es un array <br>");
-        }
-
-
-        /* Como recorrer un array de tipo asociativo. Se utiliza el bucle foreach */
-        foreach ($datos as $clave => $valor) {
-            echo "A $clave le corresponde $valor <br>";
-        }
 
         /* Como recorrer un array indexado. Se utiliza un bucle for */
         for ($i = 0; $i < 5; $i++) {
             echo ($entreSemana[$i] . "<br>");
         }
-
 
         /* Como añadir un elemento más al array indexado. */
         $entreSemana[] = "Sábado";
@@ -58,6 +41,32 @@
         for ($i = 0; $i < count($entreSemana); $i++) {
             echo ($entreSemana[$i] . "<br>");
         }
+
+
+        /**************************************/
+        /* ARRAYS ASOCIATIVOS O NO INDEXADOS  */
+        /**************************************/
+        //declaración de array asociativo.
+        $datos = array("Nombre" => "José", "Apellido" => "Rodriguez", "Edad" => 50);
+        echo $datos["Nombre"] . "<br>"; //Imprime por pantalla la posición Nombre. Es decir el valor José.
+        /*  $datos ="Juanito"; */
+
+        /* Como añadir un elemento más a un array asociativo */
+        $datos["País"] = "España";
+
+
+        //la función predefinida is_array es una función booleana que sirve para comprobar si el parámetro introducido es o no es un array.
+        if (is_array($datos) == true) {
+            echo ("Es un array <br>");
+        } else {
+            echo ("No es un array <br>");
+        }
+
+        /* Como recorrer un array de tipo asociativo. Se utiliza el bucle foreach */
+        foreach ($datos as $clave => $valor) {
+            echo "A $clave le corresponde $valor <br>";
+        }
+     
 
     ?>
 
