8fdc3fcc3e075f674424a06eb18b0779255b0882
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2023-10-27 12:23:07 +0100

    Arrays multidimensionales

---
+++ arrays_2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays multidimensionales</title>
+</head>
+
+<body>
+    <?php
+
+        /* Declaración de array multidimensional */
+        $alimento = array(
+            "fruta" => array(
+                "tropical" => "kiwi",
+                "citrico" => "mandarina",
+                "otros" => "manzana"
+            ),
+            "leche" => array(
+                "animal" => "vaca",
+                "vegetal" => "coco"
+            ),
+            "carne" => array(
+                "vacuno" => "lomo",
+                "porcino" => "pata"
+            )
+        );
+
+    ?>
+</body>
+
+</html>
