364e7c8d1853f4167905e199f47d4bf46ba06810
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2024-01-16 10:04:32 +0000

    conexion por procedimientos a BBDD

--- conexion_BBDD.php
+++ conexion_BBDD.php
@@ -14,6 +14,17 @@
         $db_password="";
 
 
+        /* Por procedimientos */
+        $conexion=mysqli_connect($db_host,$db_usuario,$db_password,$db_nombre); /* abrimos conexión con la base de datos "pruebas"*/
+        $query="SELECT * FROM datospersonales"; /*Seleccionamos toda la info de la tabla "datospersonales" */
+        $resultados=mysqli_query($conexion,$query); /*  Tabla virtual donde se almacena toda la info*/
+        $fila=mysqli_fetch_row($resultados); /* Recorre línea a línea la info que hay en la tabla virtual (result o resource o recourse) */
+
+        echo $fila[0] . " ";
+        echo $fila[1] . " ";
+        echo $fila[2] . " ";
+
+
     ?>
     
 </body>
