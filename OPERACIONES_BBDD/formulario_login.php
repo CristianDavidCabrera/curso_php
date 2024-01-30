29b6f04c3a04ebc7026dc6919fb9c35e6de6e82d
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2024-01-30 12:24:57 +0000

    Ejemplo inyeccion SQL_2

---
+++ OPERACIONES_BBDD/formulario_login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Busqueda</title>
+</head>
+
+<body>
+
+
+    <form action="pagina_datos.php" method="get">
+
+        <label>Usuario:<input type="text" name="user"></label>
+        <label>Contraseña:<input type="text" name="passwd"></label>
+        <input type="submit" name="enviado" value="Login">
+
+    </form>
+
+
+</body>
+
+</html>
---
+++ OPERACIONES_BBDD/pagina_datos.php
@@ -0,0 +1,57 @@
+<?php
+
+    $usuario=$_GET["user"];
+    $password=$_GET["passwd"];
+
+    require("datos_conexion.php");
+
+    /* -------------------------------------------------------- */
+    /*      Abrimos Conexion a base de datos                    */
+    /* -------------------------------------------------------- */
+
+    $conexion = mysqli_connect($db_host, $db_usuario, $db_password, $db_nombre);
+
+
+    /* ------------------------------------------------------- */
+    /*   Manejo de error de "no conexion con la base de datos" */
+    /* --------------------------------------------------------*/
+
+    if (mysqli_connect_errno()) {
+        echo "Fallo al conectar con la BBDD";
+        exit();
+    }
+    mysqli_select_db($conexion, $db_nombre) or die("No se encuentra la BBDD");
+
+    /* ------------------------------------------------------- */
+    /*   Codificacion de caracteres                            */
+    /* --------------------------------------------------------*/
+
+    mysqli_set_charset($conexion, "utf8"); //para trabajar con caracteres latinos.
+
+
+
+        /* -------------------------------------------------------- */
+        /*                                                          */
+        /* -------------------------------------------------------- */
+
+    
+        $consulta="SELECT * FROM usuarios where usuario = '$usuario' and contra='$password'"; 
+        $resultados=mysqli_query($conexion,$consulta); //resulset
+
+        while($fila=mysqli_fetch_array($resultados, MYSQLI_ASSOC)){ /* Búsqueda. Declaracion de Array asociativo. */
+
+            echo "Bienvenido $usuario <br> Estos son tus datos: <br>";
+
+            echo "<table><tr><td>";
+            echo $fila['usuario'] . "</td><td> ";                    /* Muestra por pantalla el registro */
+            echo $fila['contra'] . "</td><td> ";
+            echo $fila['tfno'] . "</td><td>";
+            echo $fila['direccion'] . "</td><td></tr></table>";
+            echo "<br>";
+        }
+
+        mysqli_close($conexion); /* CERRAMOS conexión con la base de datos "pruebas"  */
+
+
+?>
+    
