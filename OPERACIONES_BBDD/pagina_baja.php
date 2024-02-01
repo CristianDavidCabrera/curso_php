1f975570af5e787f65525a6deb140a8498af41b7
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2024-02-01 09:46:28 +0000

    Usamos la funcion mysqli_real_escape_string para evitar inyeccion SQL

--- OPERACIONES_BBDD/pagina_baja.php
+++ OPERACIONES_BBDD/pagina_baja.php
@@ -1,24 +1,50 @@
 <?php
 
-$usuario = $_GET["user"];
-$password = $_GET["passwd"];
+/* -------------------------------------------------------- */
+/*    Establecer datos de conexión con el servidor          */
+/* -------------------------------------------------------- */
 
 require("datos_conexion.php");
 
+/* -------------------------------------------------------- */
+/*      Abrimos Conexion a base de datos                    */
+/* -------------------------------------------------------- */
+
 $conexion = mysqli_connect($db_host, $db_usuario, $db_password, $db_nombre);
 
+/* -------------------------------------------------------- */
+/*      como evitar la INYECCION SQL                        */
+/* -------------------------------------------------------- */
+/* Para evitar la inyección SQL se utilizan la función mysqli_real_escape_string() o 
+la función mysqli_addslashes() */
+/* 
+$usuario = $_GET["user"];
+$password = $_GET["passwd"]; */
+
+$usuario = mysqli_real_escape_string($conexion, $_GET["user"]);
+$password = mysqli_real_escape_string($conexion, $_GET["passwd"]);
+
+
+
+/* -------------------------------------------------------- */
+/*     Manejo de errores en la BBDD                         */
+/* -------------------------------------------------------- */
 if (mysqli_connect_errno()) {
     echo "Fallo al conectar con la BBDD";
     exit();
 }
 mysqli_select_db($conexion, $db_nombre) or die("No se encuentra la BBDD");
 
-mysqli_set_charset($conexion, "utf8"); //para trabajar con caracteres latinos.
+/* -------------------------------------------------------- */
+/*      Trabajar con caracteres latinos.                    */
+/* -------------------------------------------------------- */
+
+mysqli_set_charset($conexion, "utf8");
 
 
-/* -------------------------------------------------------- */
-/*     LOGIN: pedir usuario y contraseña                    */
-/* -------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+/*     Pedir login (usuario y contraseña) para ELIMINAR DE LA BBDD       */
+/* ----------------------------------------------------------------------*/
 
 $consulta = "DELETE FROM usuarios where usuario = '$usuario' and contra='$password'";
 
@@ -27,6 +53,11 @@ echo "consulta<br><br>";
 if (mysqli_query($conexion, $consulta)){
     echo "Baja procesada";
 }
+
+
+/* -------------------------------------------------------- */
+/*      CERRAR Conexion con base de datos                   */
+/* -------------------------------------------------------- */
 
 mysqli_close($conexion); 
 
