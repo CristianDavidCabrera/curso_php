9bcbec04d1462709289518f12847072e62cf3a2e
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2024-04-03 07:49:32 +0100

    Conexion PDO (PHP Data Object)

---
+++ CONEXION_Y_OPERACIONES_CON_BBDD/conexion_pdo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conexion PDO</title>
+</head>
+
+<body>
+    <?php
+
+    /* ------------------------------- */
+    /*          Conexion PDO           */
+    /* ------------------------------- */
+
+    $base = new PDO('mysql:host=localhost; dbname=pruebas', 'root', '');
+
+
+
+    ?>
+
+</body>
+
+</html>
