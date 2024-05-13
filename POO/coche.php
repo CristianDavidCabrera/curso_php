ce88a55f9653d57ba29aee9906a98fc96257a97c
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2024-05-13 09:03:45 +0100

    Repaso conceptos POO. Declaración de clase e instanciación

---
+++ POO/coche.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POO Conceptos Básicos</title>
+</head>
+
+<body>
+
+    <?php
+
+    /*-------------------------------*/
+    /*  Clase. Declaración de clase  */
+    /*-------------------------------*/
+
+    class Coche
+    {
+    }
+
+
+    /*----------------------------------*/
+    /* Objeto. Declaración de instacia. */
+    /*----------------------------------*/
+
+    $obj_1 = new Coche();
+    
+    $obj_2 = new Coche();
+    
+    $obj_n = new Coche();
+
+    ?>
+
+</body>
+
+</html>
