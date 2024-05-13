acb759f97b3caea86667e151c24aab80936e3b08
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2024-05-13 11:11:39 +0100

    Actualizacion de método constructor y de la declaracion atributos Getters y Setters

--- POO/coche.php
+++ POO/coche.php
@@ -17,31 +17,120 @@
 
     class Coche
     {
+        /* NOTA */
+        /* Tras la actualización de PHP a la version 8, 
+       los atributos de clase se crean y se inicializan en el método constructor */
+
+
+        /* ------------------------------------------ */
+        /* Como se hacía ANTES de la versión PHP 8.0  */
+        /* ------------------------------------------ */
 
         /*-------------------------------*/
         /* Características o Propiedades */
         /*-------------------------------*/
+
+        /*  private int $ruedas; //Propiedad o atributo.
+        private string $color;  //Propiedad o atributo.
+        private int $motor;  //Propiedad o atributo. 
+    */
+
+        /*------------------------------------------------*/
+        /* Método constructor. Inicialización de objetos  */
+        /*------------------------------------------------*/
+
+        /*  public function __construct()
+        {
+            $this->ruedas = 4; //la palabara reservada $this hace referencia a la propia clase. En este caso a la clase Coche.
+            $this->color = "";
+            $this->motor = 1600;
+        } 
+    */
+
+        /* ------------------------------------------------- */
+        /*   Como se hace AHORA, desde la versión PHP 8.0    */
+        /* ------------------------------------------------- */
+
+        /*------------------------------------------------------*/
+        /* Método constructor y declaracion de propiedades AHORA*/
+        /*------------------------------------------------------*/
+
+        public function __construct(
+            private int $ruedas,
+            private string $color,
+            private int $motor
+        ) {
+        }
+
+        /* ---------------------------- */
+        /*  Métodos Getters y Setters.  */
+        /* ---------------------------- */
         
-        var $ruedas; //Propiedad o atributo.
-        var $color;  //Propiedad o atributo.
-        var $motor;  //Propiedad o atributo.
+        /* Los métodos Setters sirven para modificar las propiedades de los objetos o clases.  */
+        /* Los métodos Getters sirven para ver u obtener las propiedades de los objetos o clases. */
 
 
-        /*-----------------------------------------------*/
-        /* Funcionalidades o Métodos. Qué pueden hacer.  */
-        /*-----------------------------------------------*/
+        /*--------------------*/
+        /*      GETTERS       */
+        /*--------------------*/
+        public function getRuedas()
+        {
+            return $this->ruedas;
+        }
+        public function getColor()
+        {
+            return $this->color;
+        }
+        public function getMotor()
+        {
+            return $this->motor;
+        }
+
+        /*-----------------*/
+        /*     SETTERS     */
+        /*-----------------*/
+
+        public function setRuedas($ruedas)
+        {
+            $this->ruedas = $ruedas;
+        }
+        public function setColor($color)
+        {
+            $this->color = $color;
+        }
+        public function setMotor($motor)
+        {
+            $this->motor = $motor;
+        }
+
+
+        /*-----------------------------*/
+        /* Funcionalidades o Métodos.  */
+        /*-----------------------------*/
 
         function arrancar()
         {
-
+            echo "Estoy arrancando <br>";
         }
         function girar()
         {
-
+            echo "Estoy girando <br>";
         }
         function frenar()
         {
+            echo "Estoy frenando <br>";
+        }
 
+
+        public function mostrarInfo()
+        {
+
+            $info = "<h1>Información del coche:</h1>";
+            $info .= "Ruedas: " . $this->getRuedas();
+            $info .= "<br/> Color: " . $this->getColor();
+            $info .= "<br/> Motor: " . $this->getMotor();
+
+            return $info;
         }
     }
 
@@ -50,11 +139,16 @@
     /* Objeto. Declaración de instacia. */
     /*----------------------------------*/
 
-    $obj_1 = new Coche();
+    $obj_1 = new Coche("4", "Rojo", "1600"); //Declaracion de instacia e inicialización.
 
-    $obj_2 = new Coche();
+    $obj_2 = new Coche("4", "Verde", "1800");
+    $obj_n = new Coche("4", "Azul", "2000");
 
-    $obj_n = new Coche();
+
+    echo $obj_1->mostrarInfo() . "<br>";
+    echo $obj_2->mostrarInfo() . "<br>";
+    echo $obj_n->mostrarInfo() . "<br>";
+
 
     ?>
 
